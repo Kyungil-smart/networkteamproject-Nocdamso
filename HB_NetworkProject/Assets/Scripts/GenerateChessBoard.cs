e41bdabc51162bce94e8a23f2cdf043697d2728f
Nocdamso <adsnmf@gmail.com>
2026-04-27 13:27:34 +0900

    chore : 스크립트 백업

---
+++ HB_NetworkProject/Assets/Scripts/GenerateChessBoard.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class GenerateChessBoard : MonoBehaviour
+{
+    [Header("체스 타일")]
+    public GameObject DarkTile;
+    public GameObject LightTile;
+    public float TileSize = 2.3f;
+    public float TileScale = 1.0f;
+
+    private void Start()
+    {
+        GenerateBoard();
+    }
+
+    [ContextMenu("Generate Map")]
+    private void GenerateBoard()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int z = 0; z < 8; z++)
+            {
+                GameObject spawnTile = (x + z) % 2 == 0 ? DarkTile : LightTile;
+                Vector3 position = new Vector3(x * TileSize, 1, z * TileSize);
+                GameObject tile = Instantiate(spawnTile, position, Quaternion.identity);
+                
+                tile.transform.localScale = new Vector3(TileScale, TileScale, TileScale);
+                tile.transform.parent = this.transform;
+            }
+        }
+    }
+}
