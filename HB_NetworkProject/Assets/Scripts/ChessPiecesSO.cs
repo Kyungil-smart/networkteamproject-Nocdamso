7f198c44b20ea1b139002f1cc0a16d3a99ea6f52
Nocdamso <adsnmf@gmail.com>
2026-04-27 18:05:03 +0900

    Feat: 이동 로직 구현중

--- HB_NetworkProject/Assets/Scripts/ChessPiecesSO.cs
+++ HB_NetworkProject/Assets/Scripts/ChessPiecesSO.cs
@@ -15,5 +15,6 @@ public class ChessPiecesSO : ScriptableObject
     [Header("특수 이동 규칙")]
     public bool CanDoubleMoveOnFirst;       // 처음에 2칸 움직일 수 있나(폰)
     public bool CanEnPassant;               // 앙파상(폰)
+    public bool CanCaptureDiagonally;       // 폰 상대 기물 잡을 시 대각선 이동
     public bool CanCastling;                // 룩캐슬링 용도(킹, 룩)
 }
